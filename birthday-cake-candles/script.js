fc0aa9f874ee882dab2050c2dd664abc338a797a
Janssen Batista <batistajanssen.dev@gmail.com>
2022-10-27 07:41:41 -0300

    Refactoring Birthday Cake Candles challenge

--- birthday-cake-candles/script.js
+++ birthday-cake-candles/script.js
@@ -1,14 +1,21 @@
 function birthdayCakeCandles(candles) {
   let max = candles[0];
+  let totalTallestCandles = 1;
   for (let i = 1; i < candles.length; i++) {
     if (candles[i] > max) {
       max = candles[i];
+      totalTallestCandles = 1;
+      continue;
+    }
+    if (candles[i] === max) {
+      totalTallestCandles++;
     }
   }
-  let totalCandies = candles.filter((n) => n === max).length;
-  return totalCandies;
+  return totalTallestCandles;
 }
 
 // Test
-const totalCandles = birthdayCakeCandles([5, 3, 2, 2, 1, 3, 3, 5, 5, 5, 5, 5]);
-console.log(totalCandles === 6);
+const totalTallestCandles = birthdayCakeCandles([
+  5, 3, 11, 2, 10, 3, 3, 5, 5, 10, 5, 11,
+]);
+console.log(totalTallestCandles === 2);
